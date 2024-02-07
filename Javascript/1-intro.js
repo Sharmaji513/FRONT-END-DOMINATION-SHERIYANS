1f0cfd4367bb053dc900d74743e24ad5ba32c9ec
sachin <sk800968@gmail.com>
2024-02-07 20:42:51 +0530

    Javascript

---
+++ Javascript/1-intro.js
@@ -0,0 +1,29 @@
+// Topic Covered Syllabus 
+// ................................................................
+
+// variables constants
+// compilers & interpreters
+// window
+// types
+// conditionals
+// loops
+// functions
+// return
+// undefined not defined null
+// arrays
+// objects
+// questions
+
+// prototypes
+// prototypal inheritance
+// asynchronous synchronous js
+// es6 climax
+// questions
+
+// miscellaneous - switch case and ternary do-while forin forof
+
+// Interview prep
+
+// call apply bind
+// this
+// prototypal inheritance
---
+++ Javascript/2-Basics.js
@@ -0,0 +1,83 @@
+// js the language
+
+// js - Jreiden eich
+// managed by ECMA
+// naye features, puraane hatana, kharab features shi krna
+
+// js the language = code likhna seekhna, main features
+// js the dom = code likhkar cheejein banana seekhna
+
+// js the DOM - creating
+
+// Basics
+
+// basics = file connect karenge and load karnge script ko, basics of code execution, grammar and understanding of errors
+
+// var a = 12;
+// var b = 22;
+
+// console.log("Basics of JS");
+
+// kaha ho bhai chalo ghar
+// ho ghar bhai chalo kaha
+
+// grammar of js
+
+// you can have all the tools in your box & grammar is like ability to use them
+
+// you deal can me with
+// you can deal with me
+
+// var a = 12;
+
+// var = 2;
+
+// error check in console
+
+// inbult features
+
+// console
+// alert
+// prompt
+// confirm
+
+// console.log("Hello");
+// console.error("error");
+// console.warn("warning comes");
+
+// alert("message box");
+
+// var a = prompt("enter value");
+
+// console.log(a);
+
+
+
+// window 
+
+// browser mein inbuilt hota hai
+
+// there are many features which are frequently used in js and they are not the part of js, they are not the part of js the language but they are available in the browser, and when you use them in js they are called from browser and not js
+
+// window ek bucket samajh lo jismein saari wo cheejein available hai jo browser deta hai js mein use krne ke liye kyoki wo saari cheejein js ka part nahi balki browser ka part hai which is available to use in js, we can also they're provided by browser to use in js, all these features which are made available via browser are put inside window
+
+// alert
+// document
+// prompt
+// console
+// confirm
+
+console.log(window);
+
+
+// compiler vs interpreter
+
+// js language english words use krti hai and computers english nahi samjhte, wo samjhte hai 0 and 1 jisko current flow and current bypass bhi kah sakte ho, and hum chahte hai hum english mein likhke computer se baat krle, to hum ek translator lagaayege jo ki humara code english mein lega and use 0 and 1 mein convert kr dega,
+
+// compiler and interpreter translators hai jo ki english code ko convert kr rhe hai machine code mein js mein interpreter hota hai
+
+// java - compiler
+
+// jit compiler - just in time (google)
+
+// interpreter ki shuraati taakat use krta hai and compiler ki running taakat use krta hai
